679c6c5582b87354971c0a18a06bc8a05945c8f7
AnSeongSik-UE <ahnbs135@naver.com>
2025-09-23 22:59:16 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int A = 0;
+    int B = 0;
+
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / B << endl;
+    return 0;
+}
