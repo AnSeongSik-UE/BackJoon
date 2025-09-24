5d93cdfb922dd46be0a9a4c87efeaf81821a9153
AnSeongSik-UE <ahnbs135@naver.com>
2025-09-24 23:40:22 +0900

    터렛 풀이중
    https://www.acmicpc.net/problem/1002

--- main.cpp
+++ main.cpp
@@ -4,56 +4,18 @@ using namespace std;
 
 int main()
 {
-    int Dices[3] = {0,};
-    int n = 3 - 1;
-    int Double = 0;
-    int Triple = 0;
-    int Temp = 0;
 
-    cin >> Dices[0] >> Dices[1] >> Dices[2];
-    while(n > 0)
-    {
-        // 오름차순 정렬
-        for (int i = 0; i < n; ++i)
-        {
-            if (Dices[i] > Dices[i + 1])
-            {
-                Temp = Dices[i];
-                Dices[i] = Dices[i + 1];
-                Dices[i + 1] = Temp;
-            }
-        }
-        --n;
-    }
+    int X = 3;
+    int Y = 4;
+    int R = 5;
 
-    // 중복 체크
-    for (int i = 0; i < 2; ++i)
-    {
-        if (Dices[i] == Dices[i + 1])
-        {
-            if (Double == 0)
-            {
-                Double = Dices[i];
-            }
-            else if (Triple == 0)
-            {
-                Triple = Dices[i];
-            }
-        }
-    }
+    int X1 = 0;
+    int Y1 = 0;
+    int R1 = 0;
 
-    if (Triple != 0)
-    {
-        cout << 10000 + (Triple * 1000) << endl;
-    }
-    else if (Double != 0)
-    {
-        cout << 1000 + (Double * 100) << endl;
-    }
-    else
-    {
-        cout << Dices[2] * 100 << endl;
-    }
+    X1 = X * (X1 / X);
+    Y1 = Y * (Y1 / Y);
+    R1 = R * (R1 / R);
 
     return 0;
 }
