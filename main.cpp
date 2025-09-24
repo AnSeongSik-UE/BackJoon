d8c65b0bc732a49099726aea53a0c7c5b3fb4663
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-24 17:52:05 +0900

    주사위 세개 풀이 중
    주사위 세개 풀이 중

--- main.cpp
+++ main.cpp
@@ -4,25 +4,53 @@ using namespace std;
 
 int main()
 {
-    int Hour = 0;
-    int Minute = 0;
-    int CookTime = 0;
+    int Dices[3] = {0,};
+    int Double = 0;
+    int Triple = 0;
+    int Temp = 0;
 
-    cin >> Hour >> Minute >> CookTime;
+    cin >> Dices[0] >> Dices[1] >> Dices[2];
 
-    Minute += CookTime;
-
-    if (Minute >= 60 )
+    // 오름차순 정렬
+    for (int i = 0; i < 2; ++i)
     {
-        Hour += Minute / 60;
-        Minute %= 60;
-        if (Hour >= 24)
+        if (Dices[i] == Dices[i + 1])
         {
-            Hour %= 24;
+            if (Double == 0)
+            {
+                Double = Dices[i];
+            }
+            else if (Triple == 0)
+            {
+                Triple = Dices[i];
+            }
+        }
+        if (Dices[i] > Dices[i + 1])
+        {
+            Temp = Dices[i];
+            Dices[i] = Dices[i + 1];
+            Dices[i + 1] = Temp;
         }
     }
 
-    cout << Hour << " " << Minute << endl;
+    //// 중복 체크
+    //for (int i = 0; i < 2; ++i)
+    //{
+
+    //}
+
+    if (Triple != 0)
+    {
+        cout << 10000 + (Triple * 1000) << endl;
+    }
+    else if (Double != 0)
+    {
+        cout << 1000 + (Double * 100) << endl;
+    }
+    else
+    {
+        cout << Dices[2] * 100 << endl;
+    }
 
     return 0;
 }
