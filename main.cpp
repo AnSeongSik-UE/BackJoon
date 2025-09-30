a2beb5dec994deca7d56d121f90ffce2e845b4c9
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 17:35:33 +0900

    터렛 내접 외접
    터렛 내접 외접

--- main.cpp
+++ main.cpp
@@ -1,21 +1,51 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
 int main()
 {
+    int TestCase = 0;
+    double X[2] = { 0, };
+    double Y[2] = { 0, };
+    double R[2] = { 0, };
+    double DistanceOfTwo = 0;
 
-    int X = 3;
-    int Y = 4;
-    int R = 5;
+    cin >> TestCase;
+    for (int i = 0; i < TestCase; ++i)
+    {
+        cin >> X[0] >> Y[0] >> R[0] >> X[1] >> Y[1] >> R[1];
 
-    int X1 = 0;
-    int Y1 = 0;
-    int R1 = 0;
+        DistanceOfTwo = sqrt(((X[0] - X[1]) * (X[0] - X[1])) + ((Y[0] - Y[1]) * (Y[0] - Y[1])));
 
-    X1 = X * (X1 / X);
-    Y1 = Y * (Y1 / Y);
-    R1 = R * (R1 / R);
+        if (
+            X[0] == X[1] &&
+            Y[0] == Y[1] &&
+            R[0] == R[1]
+            )
+        {
+            cout << -1 << endl;
+        }
+        else if (
+            DistanceOfTwo < (R[0] + R[1]) &&
+            DistanceOfTwo > fabs((R[0] - R[1]))
+            )
+        {
+            cout << 2 << endl;
+        }
+        else if
+            (
+                DistanceOfTwo == (R[0] + R[1]) ||
+                DistanceOfTwo == fabs((R[0] - R[1]))
+                )
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 0 << endl;
+        }
+    }
 
     return 0;
 }
