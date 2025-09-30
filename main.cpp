4dfdb9ef69f4b167af7f5a8556d30f2be7d49f69
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 17:40:50 +0900

    구구단
    구구단

--- main.cpp
+++ main.cpp
@@ -1,51 +1,18 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int TestCase = 0;
-    double X[2] = { 0, };
-    double Y[2] = { 0, };
-    double R[2] = { 0, };
-    double DistanceOfTwo = 0;
+	int T = 0;
+	int A = 0;
+	int B = 0;
+	cin >> T;
 
-    cin >> TestCase;
-    for (int i = 0; i < TestCase; ++i)
-    {
-        cin >> X[0] >> Y[0] >> R[0] >> X[1] >> Y[1] >> R[1];
-
-        DistanceOfTwo = sqrt(((X[0] - X[1]) * (X[0] - X[1])) + ((Y[0] - Y[1]) * (Y[0] - Y[1])));
-
-        if (
-            X[0] == X[1] &&
-            Y[0] == Y[1] &&
-            R[0] == R[1]
-            )
-        {
-            cout << -1 << endl;
-        }
-        else if (
-            DistanceOfTwo < (R[0] + R[1]) &&
-            DistanceOfTwo > fabs((R[0] - R[1]))
-            )
-        {
-            cout << 2 << endl;
-        }
-        else if
-            (
-                DistanceOfTwo == (R[0] + R[1]) ||
-                DistanceOfTwo == fabs((R[0] - R[1]))
-                )
-        {
-            cout << 1 << endl;
-        }
-        else
-        {
-            cout << 0 << endl;
-        }
-    }
-
-    return 0;
+	for (int i = 0; i < T; ++i)
+	{
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+	return 0;
 }
