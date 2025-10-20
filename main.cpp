7556e39bcc392afb19f3c899a05b0773577257ce
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-20 13:25:46 +0900

    영수증 계산
    영수증 계산

--- main.cpp
+++ main.cpp
@@ -8,7 +8,29 @@ int main()
 	unsigned int Type = 0;
 	unsigned int Price = 0;
 	unsigned int Number = 0;
-	unsigned int TotalPrice = 0;
+	unsigned int CalcuratedRecipt = 0;
+
+	cin >> Recipt;
+	cin >> Type;
+
+	for (int i = 0; i < Type; ++i)
+	{
+		Price = 0;
+		Number = 0;
+
+		cin >> Price >> Number;
+
+		CalcuratedRecipt += (Price * Number);
+	}
+
+	if (CalcuratedRecipt == Recipt)
+	{
+		cout << "Yes" << endl;
+	}
+	else
+	{
+		cout << "No" << endl;
+	}
 
 	return 0;
 }
