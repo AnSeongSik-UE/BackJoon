5f67602578f5f9d6a9602d41b6db33baec358385
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-22 18:16:35 +0900

    주어진 수열의 최대값과 최소값 찾기
    주어진 수열의 최대값과 최소값 찾기

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
 	std::cin.tie(NULL);
-	cout.sync_with_stdio(false);
+	std::cout.sync_with_stdio(false);
 	unsigned int N = 0;
-	unsigned int X = 0;
-	unsigned int A = 0;
-	unsigned int Count = 0;
+	long InputNumber = 0;
+	long BiggerNumber = 0;
+	long LessNumber = 0;
 
-	std::cin >> N >> X;
+	std::cin >> N;
 
-	for (int i = 0; i < N; ++i)
+	std::cin >> InputNumber;
+
+	BiggerNumber = InputNumber;
+	LessNumber = InputNumber;
+	if (N != 1)
 	{
-		std::cin >> A;
-		if (A < X)
+		for (int i = 1; i < N; ++i)
 		{
-			cout << A << " ";
+			std::cin >> InputNumber;
+			if (LessNumber > InputNumber)
+			{
+				LessNumber = InputNumber;
+			}
+			else if (BiggerNumber < InputNumber)
+			{
+				BiggerNumber = InputNumber;
+			}
 		}
 	}
-	cout << "\n";
+	std::cout << LessNumber << " " << BiggerNumber << "\n";
 
 	return 0;
 }
