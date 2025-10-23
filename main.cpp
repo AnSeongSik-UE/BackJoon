9f792bbb1a3dd21f8d887995bb13e89d89ecad1b
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-23 18:43:47 +0900

    최대값과 위치 찾기
    최대값과 위치 찾기

--- main.cpp
+++ main.cpp
@@ -5,32 +5,19 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
 	unsigned int N = 0;
-	long InputNumber = 0;
-	long BiggerNumber = 0;
-	long LessNumber = 0;
+	unsigned int BiggestNumber = 0;
+	unsigned int BiggestNumbersLocation = 0;
 
-	std::cin >> N;
-
-	std::cin >> InputNumber;
-
-	BiggerNumber = InputNumber;
-	LessNumber = InputNumber;
-	if (N != 1)
+	for (int i = 0; i < 9; ++i)
 	{
-		for (int i = 1; i < N; ++i)
+		std::cin >> N;
+		if (BiggestNumber < N)
 		{
-			std::cin >> InputNumber;
-			if (LessNumber > InputNumber)
-			{
-				LessNumber = InputNumber;
-			}
-			else if (BiggerNumber < InputNumber)
-			{
-				BiggerNumber = InputNumber;
-			}
+			BiggestNumber = N;
+			BiggestNumbersLocation = i + 1;
 		}
 	}
-	std::cout << LessNumber << " " << BiggerNumber << "\n";
+	std::cout << BiggestNumber << "\n" << BiggestNumbersLocation << "\n";
 
 	return 0;
 }
