5a4581f3c1d7e550d3e218a8afd7eca8d0de55d9
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-27 18:19:46 +0900

    바구니 섞기
    바구니 섞기

--- main.cpp
+++ main.cpp
@@ -4,24 +4,38 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
-	unsigned int Number = 0;
-	unsigned int RemainderCount[42] = { 0, };
-	unsigned int Count = 0;
+	unsigned int Baskets[101] = { 0, };
 
-	for (int i = 0; i < 10; ++i)
+	unsigned int BasketNumber = 0;
+	unsigned int M = 0;
+	unsigned int FirstBasket = 0;
+	unsigned int EndBasket = 0;
+	unsigned int Sub = 0;
+	unsigned int Temp = 0;
+
+	std::cin >> BasketNumber >> M;
+
+	for (int i = 1; i <= BasketNumber; i++)
 	{
-		std::cin >> Number;
-		RemainderCount[Number % 42]++;
+		Baskets[i] = i;
 	}
 
-	for (int i = 0; i < 42; ++i)
+	for (int i = 1; i <= M; i++)
 	{
-		if (RemainderCount[i] > 0)
+		std::cin >> FirstBasket >> EndBasket;
+		Sub = EndBasket - FirstBasket;
+		for (int j = 0; j < (Sub / 2) + (Sub % 2); ++j)
 		{
-			Count++;
+			Temp = Baskets[FirstBasket + j];
+			Baskets[FirstBasket + j] = Baskets[EndBasket - j];
+			Baskets[EndBasket - j] = Temp;
 		}
 	}
-	std::cout << Count << "\n";
+
+	for (int i = 1; i <= BasketNumber; i++)
+	{
+		std::cout << Baskets[i] << " ";
+	}
 
 	return 0;
 }
