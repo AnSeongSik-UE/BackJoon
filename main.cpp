1589a312c2add4188b40d72b7c6d38cc1da810ea
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-29 18:22:08 +0900

    평균 조작
    평균 조작

--- main.cpp
+++ main.cpp
@@ -4,38 +4,27 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
-	unsigned int Baskets[101] = { 0, };
-
-	unsigned int BasketNumber = 0;
-	unsigned int M = 0;
-	unsigned int FirstBasket = 0;
-	unsigned int EndBasket = 0;
-	unsigned int Sub = 0;
-	unsigned int Temp = 0;
-
-	std::cin >> BasketNumber >> M;
-
-	for (int i = 1; i <= BasketNumber; i++)
+	unsigned int N = 0;
+	double Subjects[1000] = { 0, };
+	double  HighScore = 0;
+	double TotalScore = 0;
+	
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		Baskets[i] = i;
-	}
-
-	for (int i = 1; i <= M; i++)
-	{
-		std::cin >> FirstBasket >> EndBasket;
-		Sub = EndBasket - FirstBasket;
-		for (int j = 0; j < (Sub / 2) + (Sub % 2); ++j)
+		std::cin >> Subjects[i];
+		if (Subjects[i] > HighScore)
 		{
-			Temp = Baskets[FirstBasket + j];
-			Baskets[FirstBasket + j] = Baskets[EndBasket - j];
-			Baskets[EndBasket - j] = Temp;
+			HighScore = Subjects[i];
 		}
 	}
 
-	for (int i = 1; i <= BasketNumber; i++)
+	for (int i = 0; i < N; i++)
 	{
-		std::cout << Baskets[i] << " ";
+		TotalScore += ((Subjects[i] / HighScore) * 100);
 	}
+
+	std::cout << TotalScore/N << "\n";
 
 	return 0;
 }
