58dc229f4a91305b1097c0966d073b3c45117b6c
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-30 18:10:44 +0900

    입력 문자열의 길이
    입력 문자열의 길이

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
-	unsigned int Index = 0;
 	std::string Word = "\0";
 	
-	std::cin >> Word >> Index;
-	std::cout << Word[Index - 1] << "\n";
+	std::cin >> Word;
+	std::cout << Word.length() << "\n";
 
 	return 0;
 }
