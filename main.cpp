832fc263ae85c86acbd3c8c8439f4373beee8233
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-30 18:16:01 +0900

    입력 문자열의 앞과 뒤 출력
    입력 문자열의 앞과 뒤 출력

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
+	unsigned int TestCase = 0;
 	std::string Word = "\0";
 	
-	std::cin >> Word;
-	std::cout << Word.length() << "\n";
-
+	std::cin >> TestCase;
+	for (int i = 0; i < TestCase; i++)
+	{
+		std::cin >> Word;
+		std::cout << Word.front() << Word.back() << "\n";
+	}
 	return 0;
 }
