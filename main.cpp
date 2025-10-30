45312c2471f445c15631dfefa1763f8bf52a20f3
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-30 18:09:19 +0900

    문자열 해당 인덱스 출력
    문자열 해당 인덱스 출력

--- main.cpp
+++ main.cpp
@@ -4,27 +4,11 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
-	unsigned int N = 0;
-	double Subjects[1000] = { 0, };
-	double  HighScore = 0;
-	double TotalScore = 0;
+	unsigned int Index = 0;
+	std::string Word = "\0";
 	
-	std::cin >> N;
-	for (int i = 0; i < N; i++)
-	{
-		std::cin >> Subjects[i];
-		if (Subjects[i] > HighScore)
-		{
-			HighScore = Subjects[i];
-		}
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		TotalScore += ((Subjects[i] / HighScore) * 100);
-	}
-
-	std::cout << TotalScore/N << "\n";
+	std::cin >> Word >> Index;
+	std::cout << Word[Index - 1] << "\n";
 
 	return 0;
 }
