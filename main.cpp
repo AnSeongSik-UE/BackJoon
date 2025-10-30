2266d47e3b942d9abb234a336d44e70495eca187
AnSeongSik-unreal <ahnbs135@naver.com>
2025-10-30 18:23:07 +0900

    문자열 정수로 바꾸기 중
    문자열 정수로 바꾸기 중

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@ int main()
 {
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
-	std::string Word = "\0";
-	
-	std::cin >> Word;
-	std::cout << (int)Word.front();
+	unsigned int Count = 0;
+	std::string Numbers = "\0";
+	unsigned int TotalNumber = 0;
+
+	std::cin >> Count >>Numbers;
+	for (int i = 0; i < Count; ++i)
+	{
+		TotalNumber = (int)Numbers[i];
+	}
+
+	std::cout << TotalNumber;
+
 	return 0;
 }
