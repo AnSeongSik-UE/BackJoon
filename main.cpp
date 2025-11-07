0b811463e76347e98c79a5d71d153d67bbddba3b
AnSeongSik-unreal <ahnbs135@naver.com>
2025-11-07 18:19:55 +0900

    스택 제작 중
    스택 제작 중

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <stack>
 
 int main()
 {
-	std::cin.tie(NULL);
-	std::cout.sync_with_stdio(false);
-	unsigned int Count = 0;
-	std::string Numbers = "\0";
-	unsigned int TotalNumber = 0;
+	
+	std::stack<int>* a = new std::stack<int>;
+	a->push(1);
+	std::cout << a->top() << "\n";
+	a->push(2);
+	std::cout << a->top() << "\n";
+	a->pop();
+	std::cout << a->top() << "\n";
+	a->pop();
+	std::cout << a->top() << "\n";
 
-	std::cin >> Count >> Numbers;
-	for (int i = 0; i < Count; ++i)
-	{
-		TotalNumber += atoi(&Numbers.back());
-		Numbers.pop_back();
-	}
-
-	std::cout << TotalNumber << "\n";
-
+	delete a;
 	return 0;
 }
