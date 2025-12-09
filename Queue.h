8864aa6611d3b4918cacb9ec8f61687060bb6e70
AnSeongSik-unreal <ahnbs135@naver.com>
2025-12-09 17:57:39 +0900

    Array Queue 구현 중
    Array Queue 구현 중

--- Queue.h
+++ Queue.h
@@ -1,51 +1,81 @@
 #pragma once
 
 template<typename T>
-struct Node
-{
-	T Data;
-	Node* PreNode;
-	Node* NextNode;
-};
-
-template<typename T>
 class Queue
 {
 protected:
-	T Index;
-	Node<T>* MyNode;
+	int LastIndex = -1;
+	size_t SizeOfQ = 0;
+	size_t Capacity = 0;
+	T* Datas = nullptr;
 public:
 	Queue()
 	{
-
 	}
 	virtual ~Queue()
 	{
 
 	}
 
-	inline void Push()
+	inline void Push(T Value)
 	{
+		if (SizeOfQ >= Capacity)
+		{
+			if (Capacity == 0)
+			{
+				Capacity = 1;
+			}
+			Capacity *= 2;
+			T* NewDatas = new T[Capacity];
+			memmove(NewDatas, Datas, SizeOfQ * sizeof(T));
+			if (Datas)
+			{
+				delete[] Datas;
+			}
+			Datas = NewDatas;
+		}
 
+		++LastIndex;
+		Datas[LastIndex] = Value;
+		++SizeOfQ;
 	}
-	inline void Pop()
+	inline T Pop()
 	{
-
+		if(Datas)
+		{
+			T OutData = Datas[0];
+			--LastIndex;
+			--SizeOfQ;
+			if (SizeOfQ > 0)
+			{
+				for (int i = 0; i < SizeOfQ; ++i)
+				{
+					Datas[i] = Datas[i + 1];
+				}
+				Datas[SizeOfQ] = NULL;
+			}
+			else
+			{
+				delete[] Datas;
+			}
+			return OutData;
+		}
+		return 0;
 	}
-	inline void Size()
+	inline size_t Size()
 	{
-
+		return SizeOfQ;
 	}
 	inline void Swap()
 	{
 
 	}
-	inline void Front()
+	inline T Front()
 	{
-
+		return Datas ? Datas[0] : NULL;
 	}
-	inline void Back()
+	inline T Back()
 	{
-
+		return Datas ? Datas[LastIndex] : NULL;
 	}
 };
--- main.cpp
+++ main.cpp
@@ -4,19 +4,30 @@
 
 int main()
 {
-	std::queue<int>* a = new std::queue<int>;
-	std::queue<int>* b = new std::queue<int>;
+//	//std::queue<int>* a = new std::queue<int>;
+//	//std::queue<int>* b = new std::queue<int>;
+//
+//	//a->push(4);
+//	//a->push(5);
+//	//std::cout << a->front() << " " << a->back() << " " << a->size() << "\n";
+//	//b->push(100);
+//	//b->push(200);
+//	//std::cout << b->front() << " " << b->back() << " " << b->size() << "\n";
+//	//a->swap(*b);
+//	//b->pop();
+//	//std::cout << a->front() << " " << a->back() << " " << a->size() << "\n";
+//	//std::cout << b->front() << " " << b->back() << " " << b->size() << "\n";
 
-	a->push(4);
-	a->push(5);
-	std::cout << a->front() << " " << a->back() << " " << a->size() << "\n";
-	b->push(100);
-	b->push(200);
-	std::cout << b->front() << " " << b->back() << " " << b->size() << "\n";
-	a->swap(*b);
-	b->pop();
-	std::cout << a->front() << " " << a->back() << " " << a->size() << "\n";
-	std::cout << b->front() << " " << b->back() << " " << b->size() << "\n";
+	Queue<char>* c = new Queue<char>;
+	//Queue<int>* d;
+
+	c->Push(97);
+	c->Push(98);
+	c->Push(99);
+	std::cout << c->Front() << "\n";
+	std::cout << c->Back() << "\n";
+	c->Pop();
+	c->Pop();
 
 	return 0;
 }
